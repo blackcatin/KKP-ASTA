a9b3d63e98fc3f36771d86defd6f66824ae7102a
cardinaldeacre <cardinaldeacre@gmail.com>
2025-09-13 12:19:36 +0700

    Iqbal => UserController

---
+++ controllers/UserController.js
@@ -0,0 +1,66 @@
+// UserController.js
+const knex = require('../config/database');
+const bcrypt = require('bcrypt');
+const cryptRounds = 10;
+
+exports.register = async (req, res) => {
+	const {full_name, email, password, role} = req.body;
+	if (!full_name || !email || !password || !role) {
+		return res.status(400).json({message: 'Semua kolom harus diisi'});
+	}
+
+	try {
+		const existingUser = await knex('users').where({email}).first();
+		if (existingUser) {
+			return res.status(409).json({message: 'Email sudah terdaftar'});
+		}
+
+		const hashedPassword = await bcrypt.hash(password, cryptRounds);
+
+		await knex('users').insert({
+			full_name,
+			email,
+			password_hash: hashedPassword,
+			role,
+		});
+		res.status(201).json({message: 'Registrasi berhasil'});
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({message: 'Error server'});
+	}
+};
+
+exports.login = async (req, res) => {
+	const {email, password} = req.boody;
+
+	if (!email || !password) {
+		return res.status(400).json({message: 'Semua kolom harus diisi'});
+	}
+
+	try {
+		const user = await knex('users').where({email}).first();
+		if (!user) {
+			return res.status(400).json({message: 'Pengguna tidak ditemukan'});
+		}
+
+		const isMatch = await bcrypt.compare(password, user.password_hash);
+		if (!isMatch) {
+			return res.status(400).json({message: 'Password salah'});
+		}
+
+		delete user.password_hash;
+		return res.status(200).json({message: 'Login Berhasil'});
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({message: 'Error server'});
+	}
+};
+
+exports.index = async (req, res) => {
+	res.send('User index');
+};
+
+exports.show = async (req, res) => {
+	const {id} = req.params;
+	res.send('User show ' + id);
+};
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bcrypt = require('bcrypt');
 const knex = require('./knexfile');
 const db = require('./config/database');
+const userController = require('./controllers/UserController');
 
 // Inisialisasi Express
 const app = express();
@@ -12,12 +13,15 @@ const PORT = 3000;
 app.use(express.json());
 app.use(cors());
 
+// Run server
+app.listen(PORT, () => {
+	console.log(`Server is listening on http://localhost:${PORT}`);
+});
+
 // Routes
 app.get('/', (req, res) => {
 	res.send('Backend Server is running.');
 });
 
-// Run server
-app.listen(PORT, () => {
-	console.log(`Server is listening on http://localhost:${PORT}`);
-});
+app.post('/api/register', userController.register);
+app.post('/api/login', userController.login);
