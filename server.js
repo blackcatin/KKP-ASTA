464c83f3c227aafa951ad4056635f366f1ee8db6
cardinaldeacre <cardinaldeacre@gmail.com>
2025-10-22 21:19:58 +0700

    add autocreate directory uploads/receipts/

--- server.js
+++ server.js
@@ -8,10 +8,16 @@ const ItemController = require('./controllers/ItemController');
 const ReportController = require('./controllers/ReportController');
 const CategoryController = require('./controllers/CategoryController');
 const authMiddleware = require('./middleware/auth');
+const fs = require('fs');
 // Inisialisasi Express
 const app = express();
 const PORT = 3000;
 const path = require('path');
+
+const uploadDir = path.join(__dirname, 'uploads', 'receipts');
+if (!fs.existsSync(uploadDir)) {
+	fs.mkdirSync(uploadDir, {recursive: true});
+}
 
 // Middleware
 app.use(express.json());
